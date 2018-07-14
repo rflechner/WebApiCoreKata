38debaa1211f72697e5f2608c48b73d94dc8a465
rflechner <rflechner@romcyber.com>
2018-07-14 14:47:42 +0200

    context

---
+++ src/WebApiCoreKata/WebApiCoreKata.Persistence.SqlServer/Database/KataContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApiCoreKata.Persistence.SqlServer.Database
+{
+    /**
+    Generated with:
+    dotnet ef dbcontext scaffold "Server=BIG_ROMCY\SQLEXPRESS;Database=WebApiCoreKata;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -o Database -c KataContext
+    */
+    public partial class KataContext : DbContext
+    {
+        public KataContext()
+        {
+        }
+
+        public KataContext(DbContextOptions<KataContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=BIG_ROMCY\\SQLEXPRESS;Database=WebApiCoreKata;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
